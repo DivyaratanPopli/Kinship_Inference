4b55f4a5e06bcec6851edabe06b355aa81d927ce
divyaratan_popli <divyaratan_popli@eva.mpg.de>
2021-10-25 17:04:04 +0200

    changed from 3-4 rels together to single rel for large datasets

--- hmm_numba/looprel.sh
+++ hmm_numba/looprel.sh
@@ -1,4 +1,15 @@
 snakemake short1 --cores $n
 snakemake short2 --cores $n
 snakemake short3 --cores $n
+snakemake short4 --cores $n
+snakemake short5 --cores $n
+snakemake short6 --cores $n
+snakemake short7 --cores $n
+snakemake short8 --cores $n
+snakemake short9 --cores $n
+snakemake short10 --cores $n
+
+
+
+
 snakemake all --cores $n 
