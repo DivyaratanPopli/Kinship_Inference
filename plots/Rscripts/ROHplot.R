2a80091c2cc0743b6a9be82a1f2f008e88ad380e
divyaratan_popli <divyaratan_popli@eva.mpg.de>
2022-05-16 21:36:16 +0200

    made it high resolution

--- plots/Rscripts/ROHplot.R
+++ plots/Rscripts/ROHplot.R
@@ -15,7 +15,7 @@ make_panel1 <- function(data,acrval,leg){
     )) +
     scale_fill_manual(values = c("grey90", "white")) +
     theme_void()
-  plot1<-plot1+geom_line(aes(colour=Coverage),alpha=0.3, size=0.8)
+  plot1<-plot1+geom_line(aes(colour=Coverage),alpha=0.7, size=0.8)
   plot1<-plot1 + theme_bw()+ theme(legend.position = "none", axis.title.x = element_blank())+labs(y= "Het.") +
     scale_color_manual(values=c("0.1"="Green", "0.2"="Blue","4"="Red")) +
     theme(panel.grid.major = element_blank(),
@@ -39,8 +39,8 @@ make_panel2 <- function(data,acrval,leg){
     theme_void()
   plot2<-plot2+geom_line(aes(x=Wins, y=True_ROH, linetype="Truth"))
   #plot2
-  plot2<-plot2+geom_line(aes(x=Wins, y=Model_pred, linetype="Inferred", colour=Coverage), alpha=0.3, size=0.8)
-  
+  plot2<-plot2+geom_line(aes(x=Wins, y=Model_pred, linetype="Inferred", colour=Coverage), alpha=0.7, size=0.8)
+
   plot2<-plot2+scale_linetype_manual(values=c("Truth"="dashed", "Inferred"="solid"))
   plot2 <- plot2 + scale_color_manual(values=c("0.1"="Green", "0.2"="Blue","4"="Red"))
   if (leg==1){
@@ -57,16 +57,16 @@ make_panel2 <- function(data,acrval,leg){
   #panel<-plot_grid(plot1,plot2, labels= c("A","B"), ncol=1, rel_widths = c(1,1))
   #panel
   return(plot2)
-  
+
 }
 
 
 ROH_plot <- function(infile,outfile){
-  
-  
+
+
   data=read.csv(file = infile, header = TRUE)
   data$Coverage <- as.factor(data$Coverage)
-  
+
   chrmlist=list()
   for (val in 1:264)
   {
@@ -76,27 +76,27 @@ ROH_plot <- function(infile,outfile){
     else if (val%%2==0){
       lchr=rep(1,10)
     }
-    
+
     chrmlist <- append(chrmlist, lchr)
   }
-  
-  
+
+
   data$chrm =as.integer(chrmlist)
-  
+
   panel11 <- make_panel1(data=data, acrval='0_0_0',leg=0)
   panel21 <- make_panel1(data=data, acrval='0_0_1',leg=0)
   panel12 <- make_panel2(data=data, acrval='0_0_0',leg=0)
   panel22 <- make_panel2(data=data, acrval='0_0_1',leg=0)
-  
+
   panel31 <- make_panel1(data=data, acrval='2_0_1',leg=0)
   panel41 <- make_panel1(data=data, acrval='0_1_1',leg=0)
   panel32 <- make_panel2(data=data, acrval='2_0_1',leg=0)
   panel42 <- make_panel2(data=data, acrval='0_1_1',leg=0)
-  
+
   panel<-plot_grid(panel11,panel12,panel21,panel22,panel31,panel32,panel41,panel42, labels= c(" "," "," "," "," "," "," "," "), ncol=1, rel_widths = c(1,1,1,1,1,1,1,1), rel_heights =  c(1,1,1,1,1,1,1,1), align="v")
   panel
-  
-  
+
+
   ggsave(outfile,
          width = 10, height = 8, dpi = 150, units = "in", device='png')
 }
